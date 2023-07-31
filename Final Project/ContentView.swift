2cc73c068fd69868359477cbf8cb6b3783707c25
Amaryllis Sun <sunamaryllis@gmail.com>
2023-07-31 12:52:39 -0400

    about us page

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         
         NavigationStack {
-            Text("Stemtelligence")
+            Text("STEMtelligence")
             ZStack {
                 
                 
---
+++ Subcategory Page.swift
@@ -0,0 +1,20 @@
+//
+//  Subcategory Page.swift
+//  Final Project
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+struct Subcategory_Page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Subcategory_Page_Previews: PreviewProvider {
+    static var previews: some View {
+        Subcategory_Page()
+    }
+}
