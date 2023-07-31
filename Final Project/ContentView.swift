47b18b6b1e6298057ea1b4ed0ac6faeb1e4f3e7d
Amaryllis Sun <sunamaryllis@gmail.com>
2023-07-31 09:10:26 -0400

    Initial Commit

---
+++ Final Project/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Final Project
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationStack {
+            
+            ZStack {
+                Color(
+                
+            }
+            
+        }
+        
+        VStack {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
